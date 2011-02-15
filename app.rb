16e3db2dd402b74f5c531ca97e951ee4bb5403b4
Jeff Dewitt <jeffadewitt@gmail.com>
2011-02-14 18:05:37 -0800

    added some notes. Signed-off-by: Jeff Dewitt <jeffadewitt@gmail.com>

--- app.rb
+++ app.rb
@@ -2,8 +2,6 @@
 #TODO Remove version and cost from the password
 #TODO Ask if user wants numbers.
 #TODO Ask if user wants non alpha-numeric characters
-#TODO Filter out those the user doesn't want
-
 
 require 'bcrypt'
 require 'time'
@@ -11,17 +9,36 @@ require 'yaml'
 require 'json'
 require 'ap'
 
+# First we need to create an array of lowercase 
+# alphabet characters as strings.
 LOWER_ALPH = ("a".."z").to_a
+
+# Then we need to make an array of uppercase 
+# alphabet characters as strings.
 UPPER_ALPH = ("A".."Z").to_a
+
+# Now we generate an array of integers.
 NUMERIC = (0..9).to_a
+
+# We also need an array of characters that are ambigious.
 BAD_CHR = [1, 'l', 0, 'O', 'o','i', 'I']
+
+# Gathering each array up, and removing the bad characters
+# we get our alpha-numeric list.
 ALPH_NUM = LOWER_ALPH + UPPER_ALPH + NUMERIC - BAD_CHR
 
-
+# Now we need to find out if a password file exists.
+# The password file is in YAML format, so we also
+# need it to be parsable by the YAML interpretor.
 if File.exists?("password.yml") && YAML.parse(File.read "password.yml")
-  @database = YAML.load File.read "password.yml"
+    # Assuming the password file exists and YAML can 
+    # parse it correctly, read the file and parse the YAML.
+    @database = YAML.load File.read "password.yml"
+    # The database instance variable should now be a Hash object.
 else
-  @database = {}
+    # If YAML can't parse the file, or the file doesn't exist
+    # then we need to create a new database Hash.
+    @database = {}
 end
 
 def show_menu
@@ -37,20 +54,28 @@ def show_menu
 
     MENU
 
+    # Show the user the menu text.
     puts menu_text
 end
 
 def new_password
+    # Determine the service the user wants to create 
+    # a password to use with.
     service = service_choice
     until correct_choice? service 
         service = service_choice
     end
-    user_name = user_name service
-    generate_password service, user_name
+
+    user = user_choice
+    until correct_choice? user 
+        user = user_choice
+    end
+    # user_name = user_name service
+    generate_password service, user
 end
 
 def service_choice
-    puts "What is this password for? "
+    puts "What service would you like to do this for? "
     gets.chomp.downcase
 end
 
@@ -60,11 +85,18 @@ def correct_choice? service
     possible_choices = ["yes", "yep","sure", "y", "yeah", "ok", ""]
     possible_choices.include? choice
 end
-    
+
+def user_choice
+    puts "What user name would you like to do this for? "
+    gets.chomp.downcase
+end
+=begin
 def user_name service
     puts "Please enter your user name for #{service}: "
     gets.chomp.downcase
 end
+=end
+
 
 def generate_password service, user_name
     puts "Here is your Password for #{user_name} on #{service}: "
@@ -99,14 +131,51 @@ def show_password
     puts @database.to_yaml
 end
 
-def 
-
 def delete_password
+    services = @database.keys
+    for service in services do
+        number = services.index(service) + 1
+        puts "#{number}. #{service}"
+    end
+    service = service_choice
+    until correct_choice? service 
+        service = service_choice
+    end
 
+    users = @database[service].keys
+    for user in users do
+        number = services.index(service) + 1
+        puts "#{number}. #{service}"
+    end
+    user = user_choice
+    until correct_choice? user 
+        user = user_choice
+    end
 end
 
-def check_for_password
-    
+def get_crypted_master_password
+    BCrypt::Password.new @database[:master]
+end
+
+def generate_new_master_password
+    puts 'You need to create a master password for this program to use:'
+    master_password = gets.chomp!
+    @database.merge! master: BCrypt::Password.create(master_password, :cost => 10).to_s
+end
+
+def ask_for_master_password
+    puts 'Please enter your Master Password:'
+    gets.chomp!
+end
+
+def verify_master_password entered, crypted
+    unless crypted == entered
+        puts 'Incorect password entered'
+        false
+    else
+        true
+    end
+end
 
 def menu_input input
     option = input.chomp.to_i
@@ -119,9 +188,16 @@ def menu_input input
 end
 
 def secure_connection
-    check_for_password
-    crypted_password = reteive_password
-    verrify_password gets, cryted_password
+    if @database.keys.include? :master
+        validated = false
+        until validated
+            crypted = get_crypted_master_password
+            entered = ask_for_master_password
+            validated = verify_master_password entered, crypted
+        end
+    else
+        generate_new_master_password
+    end
 end
 
 secure_connection
